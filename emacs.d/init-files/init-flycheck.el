aed393041706616072608078f749344489c076bb
Bryan Bennett <Bryan.Bennett@gtri.gatech.edu>
2015-11-13 13:39:30 -0500

    Add pylama checker to flycheck

--- emacs.d/init-files/init-flycheck.el
+++ emacs.d/init-files/init-flycheck.el
@@ -1,3 +1,14 @@
+(flycheck-define-checker python-pylama
+  "A Python syntax and style checker using the pylama utility. See URL
+  `http://pypi.python.org/pypi/pylama'."
+
+  :command ("pylama" source-inplace)
+  :error-patterns
+  ((error line-start (file-name) ":" line ":" (message) line-end))
+  :modes python-mode)
+
+(add-to-list 'flycheck-checkers 'python-pylama)
+
 (add-hook 'after-init-hook #'global-flycheck-mode)
 (add-to-list 'display-buffer-alist
              `(,(rx bos "*Flycheck errors*" eos)
