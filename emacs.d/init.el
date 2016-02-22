70ea926fe9dac7676b8b18f151278117ba424d64
Bryan Bennett <bbenne10@gmail.com>
2016-02-22 08:43:46 -0500

    Emacs: Swap font to match dwm config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -10,8 +10,9 @@
 (load-file "~/.emacs.d/langs.el")
 
 ; change how emacs looks
-(set-frame-font "Input-10")
-(setq default-frame-alist '((font . "Input-10")))
+(set-frame-font "Knack-10")
+(setq default-frame-alist '((font . "Knack-10")))
+
 (if (daemonp)
   (add-hook 'after-make-frame-functions
             (lambda (frame) (with-selected-frame frame (load-theme 'material t))))
