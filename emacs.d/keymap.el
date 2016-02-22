d25d148eae12d33c8f9d61d7a76e3ddcbf542987
Bryan Bennett <bbenne10@gmail.com>
2016-02-22 08:44:48 -0500

    Emacs: Add Anaconda Mode

--- emacs.d/keymap.el
+++ emacs.d/keymap.el
@@ -2,7 +2,25 @@
   (interactive)
   (cond (get-buffer-window "*Flycheck errors*" "visible")
         (delete-window (get-buffer-window "*Flycheck-errors*" "visible")))
-  )
+)
+
+(defun python-insert-trace ()
+  ;; insert a line that imports pdb and sets a trace just below the current line
+  (interactive)
+  (move-end-of-line 1)
+  (insert "\n")
+  (indent-according-to-mode)
+  (insert "import pdb; pdb.set_trace()")
+  ;;(back-to-indentation)
+)
+
+(defun setup-python-keybinds ()
+  (define-key evil-normal-state-map (kbd "C-c g") 'anaconda-mode-find-definitions)
+  (define-key evil-normal-state-map (kbd "C-c a") 'anaconda-mode-find-assignments)
+  (define-key evil-normal-state-map (kbd "C-c r") 'anaconda-mode-find-references)
+  (define-key evil-normal-state-map (kbd "C-c ?") 'anaconda-mode-show-doc)
+  (define-key evil-normal-state-map (kbd "C-c t") 'python-insert-trace)
+)
 
 (define-key evil-normal-state-map (kbd "<SPC>lo") 'flycheck-list-errors)
 (define-key evil-normal-state-map (kbd "<SPC>lc") 'delete-flycheck-errors-list)
