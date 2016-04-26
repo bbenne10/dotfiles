df25c250fbff0f41409039147369b87842360a17
Bryan Bennett <bbenne10@gmail.com>
2016-04-26 10:07:55 -0400

    fresh: include bootstrap and install_arch scripts

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+if [ "$PWD" != "$HOME/.dotfiles" ]; then
+  printf "==> Dotfiles must be cloned into ~/.dotfiles; exiting...\n"
+  exit
+fi
+
+if [ ! -d "$HOME/.fresh" ]; then
+  printf "==> Installing Fresh\n" 
+  git clone https://github.com/freshshell/fresh ~/.fresh/source/freshshell/fresh > /dev/null
+fi
+
+if [ ! -r "$HOME/.freshrc" ]; then
+  printf "==> Linking freshrc\n"
+  ln -s freshrc "$HOME/.freshrc"
+fi
+
+"$HOME"/.fresh/source/freshshell/fresh/bin/fresh
