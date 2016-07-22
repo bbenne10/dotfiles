9bf43b87069c04294b9d5eae95fe9b9bd861b2f6
Bryan Bennett <bbenne10@gmail.com>
2016-07-22 17:05:12 -0400

    Emacs: Update to match work

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -5,7 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("4f5bb895d88b6fe6a983e63429f154b8d939b4a8c581956493783b2515e22d6d" "ad950f1b1bf65682e390f3547d479fd35d8c66cafa2b8aa28179d78122faa947" default)))
+    ("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "4f5bb895d88b6fe6a983e63429f154b8d939b4a8c581956493783b2515e22d6d" "ad950f1b1bf65682e390f3547d479fd35d8c66cafa2b8aa28179d78122faa947" default)))
+ '(package-selected-packages
+   (quote
+    (color-theme-sanityinc-tomorrow ## markdown-preview-eww markdown-preview-mode markdown-mode evil-mu4e web-mode go-mode avy evil-vimish-fold vimish-fold persp-projectile perspective grizzl helm-projectile yafolding nm pyenv-mode company-anaconda company spaceline smooth-scrolling puppet-mode projectile flx-ido flycheck fic-mode evil-surround evil-matchit evil dired+ diminish anaconda-mode ample-theme)))
  '(term-file-aliases
    (quote
     (("st-256color" . "xterm-256color")
--- emacs.d/packages.d/projectile.el
+++ emacs.d/packages.d/projectile.el
@@ -1,5 +1,4 @@
 (setq projectile-enable-caching t)
-; (setq projectile-require-project-root nil)
-; (setq projectile-completion-system 'grizzl)
+(setq projectile-completion-system 'grizzl)
 
 (projectile-global-mode)
