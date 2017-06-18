512e651f9a58a470eae3a7ae2a7cf32dde6317e8
Bryan Bennett <bbenne10@gmail.com>
2017-06-18 10:26:49 -0400

    emacs: Add hasklig font mappings to emacs; switch font to hasklig

--- emacs.d/ui.el
+++ emacs.d/ui.el
@@ -1,7 +1,28 @@
-(load "~/.emacs.d/fira_init.el")
-
-;; (set-frame-font "Fira Code:size=10")
 (setq default-frame-alist '((font . "Fira Code:size=12")))
+(defun my-correct-symbol-bounds (pretty-alist)
+  (mapcar (lambda (el)
+            (setcdr el (string ?\t (cdr el)))
+            el)
+          pretty-alist))
+(defun my-ligature-list (ligatures codepoint-start)
+  (let ((codepoints (-iterate '1+ codepoint-start (length ligatures))))
+    (-zip-pair ligatures codepoints)))
+(setq my-hasklig-ligatures
+      (let* ((ligs '("&&" "***" "*>" "\\\\" "||" "|>" "::"
+                     "==" "===" "==>" "=>" "=<<" "!!" ">>"
+                     ">>=" ">>>" ">>-" ">-" "->" "-<" "-<<"
+                     "<*" "<*>" "<|" "<|>" "<$>" "<>" "<-"
+                     "<<" "<<<" "<+>" ".." "..." "++" "+++"
+                     "/=" ":::" ">=>" "->>" "<=>" "<=<" "<->")))
+        (my-correct-symbol-bounds (my-ligature-list ligs #Xe100))))
+
+(defun my-set-hasklig-ligatures ()
+  (setq prettify-symbols-alist
+        (append my-hasklig-ligatures prettify-symbols-alist))
+  (prettify-symbols-mode))
+
+(add-hook 'prog-mode-hook 'my-set-hasklig-ligatures)
+
 (scroll-bar-mode 0)
 (menu-bar-mode 0)
 (tool-bar-mode 0)
