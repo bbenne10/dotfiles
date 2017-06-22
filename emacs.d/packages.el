7d7117d4871f0ba0347741608e6402a6d71a38f1
Bryan Bennett <bbenne10@gmail.com>
2017-06-22 10:20:23 -0400

    emacs: clean up package-list

--- emacs.d/packages.el
+++ emacs.d/packages.el
@@ -1,10 +1,12 @@
 (package-initialize)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
-  (package-install 'use-package))
+  (package-install 'use-package)
+)
 
 (setq use-package-always-ensure t)
 (eval-when-compile
