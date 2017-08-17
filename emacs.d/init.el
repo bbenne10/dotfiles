75624bb8035eaa8a227f215c4506e314916187b1
Bryan Bennett <bbenne10@gmail.com>
2017-08-17 12:49:23 -0400

    emacs: increase gc threshold on startup

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,15 +1,7 @@
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
-(require 'cl)
+(setq gc-cons-threshold 64000000)
+(add-hook 'after-init-hook #'(lambda () (setq gc-cons-threshold 800000)))
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file 'noerror)
-
-(load "~/.emacs.d/base.el")
 (load "~/.emacs.d/packages.el")
 (load "~/.emacs.d/ui.el")
 (load "~/.emacs.d/languages.el")
