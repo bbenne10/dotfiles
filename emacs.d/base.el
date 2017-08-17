dda396e5c2433a7122b8dbbc73f40c9d40115f45
Bryan Bennett <bbenne10@gmail.com>
2017-08-17 10:18:32 -0400

    emacs: Set clipboard copying with newer vars

--- emacs.d/base.el
+++ emacs.d/base.el
@@ -13,6 +13,6 @@
 (prettify-symbols-mode)
 
 ;; Enable yanking and pasting to and from both clipboards
-(setq x-select-enable-clipboard t)
-(setq x-select-enable-primary t)
+(setq select-enable-clipboard t)
+(setq select-enable-primary t)
 (setq mouse-drag-copy-region t)
