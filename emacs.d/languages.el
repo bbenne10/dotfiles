771f60c24260483411c4733d075cb4a993028380
Bryan Bennett <bbenne10@gmail.com>
2017-11-26 16:20:04 -0500

    Switch to using org-babel-mode to build dotfiles into $HOME
    Use `./install.sh` to run once you've gotten emacs installed.

--- emacs.d/languages.el
+++
@@ -1,9 +0,0 @@
-(add-hook 'js-mode-hook
-          (function (lambda ()
-                      (flycheck-select-checker 'javascript-standard)
-                      (setq js-indent-level 2)
-                      (setq evil-shift-width 2))))
-
-(add-hook 'c++-mode-hook
-          (function (lambda ()
-                      (flycheck-select-checker 'c/c++-gcc))))
