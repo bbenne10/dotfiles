e04371ef84c6ab6d02ca8e1a512ccdc1ba0197c8
Bryan Bennett <bbenne10@gmail.com>
2017-12-31 09:16:25 -0500

    Flesh out install script to install otherwise unmanageable deps

--- install.sh
+++ install.sh
@@ -1,2 +1,75 @@
 #!/bin/sh
-find . -name "*.org" -exec emacs --batch -l org --eval "(org-babel-tangle-file \"{}\")" \; 2>&1 | grep "^Tangled"
+
+BIN_DIR="$HOME/.bin"
+BUILD_DIR="../"  # assume we want builds/ alongside our dotfiles repo
+
+SRANDRD_URI="git@github.com:bbenne10/srandrd.git"
+XINPUTD_URI="git@github.com:bbenne10/xinputd.git"
+DWM_URI="git@github.com:bbenne10/dwm.git"
+DWMSTATUS_URI="git@github.com:bbenne10/dwmstatus.git"
+XSS_URI="git@github.com:9wm/xss.git"
+
+error () {
+  printf "%s\n" "$*" >&2;
+}
+
+install_generic () {
+  uri="$1"
+  target="$2"
+
+  printf "Installing %s\n" "$target"
+
+  git clone "$uri" "$target"
+  cd "$target"
+    if make; then
+        cp "$target" "$BIN_DIR"
+    else
+      error "Failed to build $target."
+    fi
+  cd -
+}
+
+install_srandrd () {
+  install_generic "$SRANDRD_URI" srandrd
+}
+
+install_xinputd () {
+  install_generic "$XINPUTD_URI" xinputd
+}
+
+install_xss () {
+  install_generic "$XSS_URI" xss
+}
+
+install_dwm () {
+  install_generic "$DWM_URI" dwm
+}
+
+install_dwmstatus () {
+  install_generic "$DWMSTATUS_URI" dwmstatus
+}
+
+tangle_dotfiles () {
+  find . -name "*.org" -exec emacs --batch -l org --eval "(org-babel-tangle-file \"{}\")" \; 2>&1 | grep "^Tangled"
+}
+
+main () {
+  test -d "$BIN_DIR" || mkdir -p "$BIN_DIR"
+  test -d "$BUILD_DIR" || mkdir -p "$BUILD_DIR"
+  start_dir="$PWD"
+
+  cd "$BUILD_DIR"
+    install_srandrd
+    install_xinputd
+    install_xss
+    install_dwm
+    install_dwmstatus
+  cd "$start_dir"
+
+  tangle_dotfiles
+
+  test -L $HOME/.xinitrc || test -F $HOME/.xinitrc && rm $HOME/.xinitrc
+  ln -s $HOME/.xsession $HOME/.xinitrc
+}
+
+main
